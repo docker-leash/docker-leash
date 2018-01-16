be09587b913c64d75dfa6e201c29590c1b1433f3
Mathieu Alorent <github@kumy.net>
2018-01-16 16:50:23 +0100

    Simplify "checks" imports

--- docker_leash/checks/__init__.py
+++ docker_leash/checks/__init__.py
@@ -1,11 +1,7 @@
 # vim:set ts=4 sw=4 et:
 
-__all__ = [
-    'Deny',
-    'Allow',
-]
-
 from .allow import Allow
-from .allow import Allow as ContainerName
 from .deny import Deny
 from .bind_volumes import BindVolumes
+
+ContainerName = Allow
