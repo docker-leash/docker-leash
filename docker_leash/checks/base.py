f2b83ac8ad643d7e7204b72f710d77ea1f3c16fd
Mathieu Alorent <github@kumy.net>
2018-01-17 18:06:38 +0100

    Add a function to replace $USER by connected username

--- docker_leash/checks/base.py
+++ docker_leash/checks/base.py
@@ -4,6 +4,8 @@ Base
 ----
 '''
 
+import re
+
 
 class BaseCheck(object):
     """The :class:`BaseCheck` class is the base class for all the checks
@@ -30,3 +32,70 @@ class BaseCheck(object):
         raise NotImplementedError(
             "'run' mot implemented in module `%s`" % self.__class__.__name__
         )
+
+    @staticmethod
+    def replace_user(value, payload):
+        """A helper function to replace $USER in string
+
+        If exact match is found, the replace is done by the value of the current
+        connected user. If no user defined, then it return `None`. If '$' is
+        preceded by a '\\' (backslash), then no substritituion is done.
+
+        .. code-block:: pycon
+
+            >>> payload = payload({"User": "mal"})
+            >>> BaseCheck.replace_user("$USER-loves-me", payload)
+            mal-loves-me
+
+        .. code-block:: pycon
+
+            >>> payload = payload({"User": "mal"})
+            >>> BaseCheck.replace_user("\$USER-loves-me", payload)
+            $USER-loves-me
+
+        .. code-block:: pycon
+
+            >>> payload = payload({"User": None})
+            >>> BaseCheck.replace_user("$USER-loves-me", payload)
+            # Return None
+
+            >>> payload = payload({})
+            >>> BaseCheck.replace_user("$USER-loves-me", payload)
+            # Return None
+
+        It works with `list` too:
+
+        .. code-block:: pycon
+
+            >>> a = ["who-loves-you", "$USER-loves-me", "\$USER-loves-me"]
+            >>> payload = payload({"User": "mal"})
+            >>> BaseCheck.replace_user("$USER-loves-me", payload)
+            ["who-loves-you", "mal-loves-me", "\$USER-loves-me"]
+
+        :param value: The input to replace
+        :type value: string or list or None
+        :param payload: The payload of the current request.
+        :type payload: :class:`docker_leash.payload.Payload`
+        :return: The replaced value(s)
+        :rtype: string or list or None
+        """
+
+        def replace(value, new):
+            return re.sub(
+                r"(?<!\\)(?:\\\\)?\$USER",
+                new,
+                value
+            )
+            # return value.replace('$USER', new)
+
+        if isinstance(value, str):
+            if payload.user is None:
+                return None
+            return replace(value, payload.user)
+
+        result = []
+        for val in value:
+            if payload.user is None:
+                continue
+            result.append(replace(val, payload.user))
+        return result
--- tests/checks/test_basecheck.py
+++ tests/checks/test_basecheck.py
@@ -1,14 +1,110 @@
 # vim:set ts=4 sw=4 et:
 
+'''
+BaseCheckTests
+----
+'''
+
 import unittest
 
 from docker_leash.checks.base import BaseCheck
 from docker_leash.config import Config
+from docker_leash.payload import Payload
 
 
 class BaseCheckTests(unittest.TestCase):
+    """Validate :class:`docker_leash.checks.BaseCheck` features
+    """
 
     def test_init(self):
+        """Check that :meth:`docker_leash.checks.BaseCheck.run` raise
+        :exc:`NotImplementedError`
+        """
         base = BaseCheck()
         with self.assertRaises(NotImplementedError):
             base.run(Config(), {})
+
+    def test_replace_user_string(self):
+        """Check that :meth:`docker_leash.checks.BaseCheck.replace_user` can
+        replace user on `string`
+        """
+        base = BaseCheck()
+
+        payload = Payload({"User": "mal"})
+        result = BaseCheck.replace_user("$USER-loves-me", payload)
+        self.assertEqual(result, "mal-loves-me")
+
+        result = BaseCheck.replace_user("$USERNAME-loves-me", payload)
+        self.assertEqual(result, "malNAME-loves-me")
+
+        result = BaseCheck.replace_user("do-you-think-$USER-loves-me", payload)
+        self.assertEqual(result, "do-you-think-mal-loves-me")
+
+        result = BaseCheck.replace_user("$USER-is-$USER", payload)
+        self.assertEqual(result, "mal-is-mal")
+
+        payload = Payload({"User": "rda"})
+        result = BaseCheck.replace_user("$USER-loves-me", payload)
+        self.assertEqual(result, "rda-loves-me")
+
+    def test_replace_user_list(self):
+        """Check that :meth:`docker_leash.checks.BaseCheck.replace_user` can
+        replace user on `list`
+        """
+        base = BaseCheck()
+        values = [
+            "$USER-loves-me",
+            "$USERNAME-loves-me",
+            "do-you-think-$USER-loves-me",
+            "of-course"
+        ]
+        attended = [
+            "mal-loves-me",
+            "malNAME-loves-me",
+            "do-you-think-mal-loves-me",
+            "of-course"
+        ]
+
+        payload = Payload({"User": "mal"})
+        result = BaseCheck.replace_user(values, payload)
+        self.assertListEqual(result, attended)
+
+    def test_replace_user_as_anonymous(self):
+        """Check that :meth:`docker_leash.checks.BaseCheck.replace_user` returns
+        `None` when connected as anonymous
+        """
+        base = BaseCheck()
+
+        payload = Payload({"User": None})
+        result = BaseCheck.replace_user("$USER-loves-me", payload)
+        self.assertIsNone(result)
+
+        payload = Payload({})
+        result = BaseCheck.replace_user("$USER-loves-me", payload)
+        self.assertIsNone(result)
+
+    def test_replace_user_with_escape(self):
+        """Check that :meth:`docker_leash.checks.BaseCheck.replace_user` don't
+        replace when '$' is escaped
+        """
+        base = BaseCheck()
+
+        payload = Payload({"User": "mal"})
+        result = BaseCheck.replace_user(r"\$USER-loves-me", payload)
+        self.assertEqual(result, r"\$USER-loves-me")
+
+        result = BaseCheck.replace_user([r"\$USER-loves-me"], payload)
+        self.assertEqual(result, [r"\$USER-loves-me"])
+
+        result = BaseCheck.replace_user([r"\\$USER-loves-me"], payload)
+        self.assertEqual(result, [r"\mal-loves-me"])
+
+        result = BaseCheck.replace_user([r"\\\$USER-loves-me"], payload)
+        self.assertEqual(result, [r"\$USER-loves-me"])
+
+        payload = Payload({"User": None})
+        result = BaseCheck.replace_user(r"\$USER-loves-me", payload)
+        self.assertEqual(result, r"\$USER-loves-me")
+
+        result = BaseCheck.replace_user([r"\$USER-loves-me"], payload)
+        self.assertEqual(result, [r"\$USER-loves-me"])
